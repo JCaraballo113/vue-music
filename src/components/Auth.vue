398387c78a908b06bdd13f77546f0207c08ab987
John Caraballo <jcaraballo019@gmail.com>
2021-12-26 01:15:06 -0500

    default values

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -75,7 +75,12 @@
             </button>
           </form>
           <!-- Registration Form -->
-          <vee-form v-show="tab === 'register'" :validation-schema="schema" @submit="register">
+          <vee-form
+            v-show="tab === 'register'"
+            :validation-schema="schema"
+            @submit="register"
+            :initial-values="userData"
+          >
             <!-- Name -->
             <div class="mb-3">
               <label class="inline-block mb-2">Name</label>
@@ -191,6 +196,9 @@ export default {
         confirm_password: 'confirmed:@password',
         country: 'required|excluded:Antartica',
         tos: 'required',
+      },
+      userData: {
+        country: 'USA',
       },
     };
   },
