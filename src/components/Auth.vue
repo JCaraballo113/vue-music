9d5433d868e8eeedcf5b9d9c038efc8bc68d86cc
John Caraballo <jcaraballo019@gmail.com>
2021-12-26 01:13:15 -0500

    multiple error messages

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -111,12 +111,17 @@
             <!-- Password -->
             <div class="mb-3">
               <label class="inline-block mb-2">Password</label>
-              <vee-field
-                type="password"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Password"
-                name="password"
-              />
+              <vee-field name="password" :bails="false" #default="{ field, errors }">
+                <input
+                  type="password"
+                  class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+                  placeholder="Password"
+                  v-bind="field"
+                />
+                <div class="text-red-600" v-for="error in errors" :key="error">
+                  {{ error }}
+                </div>
+              </vee-field>
               <ErrorMessage class="text-red-600" name="password" />
             </div>
             <!-- Confirm Password -->
