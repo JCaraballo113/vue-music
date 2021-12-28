e73b1fe7f455ccdd737696e95bd56997bd95b169
John Caraballo <jcaraballo019@gmail.com>
2021-12-27 22:20:52 -0500

    Refactor to forms

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,56 @@
+<template>
+  <!-- Login Form -->
+  <vee-form @submit="login" :validation-schema="loginSchema">
+    <!-- Email -->
+    <div class="mb-3">
+      <label class="inline-block mb-2">Email</label>
+      <vee-field
+        type="email"
+        name="email"
+        class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+        placeholder="Enter Email"
+      />
+      <ErrorMessage class="text-red-600" name="email" />
+    </div>
+    <!-- Password -->
+    <div class="mb-3">
+      <label class="inline-block mb-2">Password</label>
+      <vee-field
+        name="password"
+        type="password"
+        class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+        placeholder="Password"
+      />
+      <ErrorMessage class="text-red-600" name="password" />
+    </div>
+    <button
+      type="submit"
+      class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition hover:bg-purple-700"
+    >
+      Submit
+    </button>
+  </vee-form>
+</template>
+
+<script>
+export default {
+  name: 'LoginForm',
+  data() {
+    return {
+      loginSchema: {
+        email: 'required|email',
+        password: 'required|min:3|max:32',
+      },
+      reg_in_submission: false,
+      reg_show_alert: false,
+      reg_alert_variant: 'bg-blue-500',
+      reg_alert_msg: 'Please wait! Your account is being created',
+    };
+  },
+  methods: {
+    login(values) {
+      console.log(values);
+    },
+  },
+};
+</script>
