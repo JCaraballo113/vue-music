6bb3ae69019e8f81511188e6dccf518ff538aa9f
John Caraballo <jcaraballo019@gmail.com>
2022-01-01 15:25:14 -0500

    named routes

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,7 +6,7 @@
       <router-link
         exact-active-class="no-active"
         class="text-white font-bold uppercase text-2xl mr-4"
-        to="/"
+        :to="{ name: 'home' }"
         >Music</router-link
       >
 
@@ -15,7 +15,7 @@
         <ul class="flex flex-row mt-1">
           <!-- Navigation Links -->
           <li>
-            <router-link class="px-2 text-white" to="/about">About</router-link>
+            <router-link :to="{ name: 'about' }" class="px-2 text-white">About</router-link>
           </li>
           <li v-if="!user">
             <a class="px-2 text-white" href="#" @click.prevent="toggleAuthModal"
@@ -24,7 +24,7 @@
           </li>
           <template v-else>
             <li>
-              <router-link class="px-2 text-white" to="/manage">Manage</router-link>
+              <router-link class="px-2 text-white" :to="{ name: 'manage' }">Manage</router-link>
             </li>
             <li>
               <a class="px-2 text-white" href="#" @click.prevent="logout">Logout</a>
