deba53748a07a6c4079e33c13fa93b7264c424c1
John Caraballo <jcaraballo019@gmail.com>
2023-11-20 09:59:32 -0500

    add localization

--- src/components/SongDetail.vue
+++ src/components/SongDetail.vue
@@ -1,94 +1,98 @@
 <template>
-  <section class="w-full mb-8 py-14 text-center text-white relative">
-    <div
-      class="absolute inset-0 w-full h-full box-border bg-contain music-bg"
-      style="background-image: url(/assets/img/song-header.png)"
-    ></div>
-    <div class="container mx-auto flex items-center">
-      <!-- Play/Pause Button -->
-      <button
-        type="button"
-        class="z-50 h-24 w-24 text-3xl bg-white text-black rounded-full focus:outline-none"
-        @click="newSong(song)"
-      >
-        <i class="fas" :class="{ 'fa-play': !playing, 'fa-pause': playing }"></i>
-      </button>
-      <div class="z-50 text-left ml-8">
-        <!-- Song Info -->
-        <div class="text-3xl font-bold">{{ song?.modified_name }}</div>
-        <div>{{ song?.genre }}</div>
-      </div>
-    </div>
-  </section>
-  <!-- Form -->
-  <section class="container mx-auto mt-6" id="comments">
-    <div class="bg-white rounded border border-gray-200 relative flex flex-col">
-      <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
-        <!-- Comment Count -->
-        <span class="card-title">Comments ({{ song?.comment_count ?? 0 }})</span>
-        <i class="fa fa-comments float-right text-green-400 text-2xl"></i>
-      </div>
-      <div class="p-6">
-        <div
-          class="text-white text-center font-bold p-4 mb-4"
-          v-if="commentFormState.showAlert"
-          :class="commentFormState.alertVariant"
+  <main>
+    <section class="w-full mb-8 py-14 text-center text-white relative">
+      <div
+        class="absolute inset-0 w-full h-full box-border bg-contain music-bg"
+        style="background-image: url(/assets/img/song-header.png)"
+      ></div>
+      <div class="container mx-auto flex items-center">
+        <!-- Play/Pause Button -->
+        <button
+          type="button"
+          class="z-50 h-24 w-24 text-3xl bg-white text-black rounded-full focus:outline-none"
+          @click="newSong(song)"
         >
-          {{ commentFormState.alertMessage }}
+          <i class="fas" :class="{ 'fa-play': !playing, 'fa-pause': playing }"></i>
+        </button>
+        <div class="z-50 text-left ml-8">
+          <!-- Song Info -->
+          <div class="text-3xl font-bold">{{ song?.modified_name }}</div>
+          <div>{{ song?.genre }}</div>
         </div>
-        <VeeForm :validation-schema="schema" @submit="submitComment">
-          <VeeField
-            as="textarea"
-            name="comment"
-            class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded mb-4"
-            placeholder="Your comment here..."
-          ></VeeField>
-          <ErrorMessage name="comment" class="text-red-600" />
-          <button
-            type="submit"
-            class="py-1.5 px-3 rounded text-white bg-green-600 block"
-            :disabled="commentFormState.inSubmission"
+      </div>
+    </section>
+    <!-- Form -->
+    <section class="container mx-auto mt-6" id="comments">
+      <div class="bg-white rounded border border-gray-200 relative flex flex-col">
+        <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
+          <!-- Comment Count -->
+          <span class="card-title">{{
+            $tc('song.comment_count', song?.comment_count ?? 0, { count: song?.comment_count ?? 0 })
+          }}</span>
+          <i class="fa fa-comments float-right text-green-400 text-2xl"></i>
+        </div>
+        <div class="p-6">
+          <div
+            class="text-white text-center font-bold p-4 mb-4"
+            v-if="commentFormState.showAlert"
+            :class="commentFormState.alertVariant"
           >
-            Submit
-          </button>
-        </VeeForm>
-        <!-- Sort Comments -->
-        <select
-          class="block mt-4 py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-          v-model.number="sortBy"
-        >
-          <option value="1">Latest</option>
-          <option value="2">Oldest</option>
-        </select>
+            {{ commentFormState.alertMessage }}
+          </div>
+          <VeeForm :validation-schema="schema" @submit="submitComment">
+            <VeeField
+              as="textarea"
+              name="comment"
+              class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded mb-4"
+              placeholder="Your comment here..."
+            ></VeeField>
+            <ErrorMessage name="comment" class="text-red-600" />
+            <button
+              type="submit"
+              class="py-1.5 px-3 rounded text-white bg-green-600 block"
+              :disabled="commentFormState.inSubmission"
+            >
+              Submit
+            </button>
+          </VeeForm>
+          <!-- Sort Comments -->
+          <select
+            class="block mt-4 py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+            v-model.number="sortBy"
+          >
+            <option value="1">Latest</option>
+            <option value="2">Oldest</option>
+          </select>
+        </div>
       </div>
-    </div>
-  </section>
-  <!-- Comments -->
-  <ul class="container mx-auto" v-if="sortedComments.length > 0">
-    <li
-      class="p-6 bg-gray-50 border border-gray-200"
-      v-for="comment in sortedComments"
-      :key="comment.id"
-    >
-      <!-- Comment Author -->
-      <div class="mb-5">
-        <div class="font-bold">{{ comment.users?.name }}</div>
-        <time
-          >{{
-            Math.floor((Date.now() - new Date(comment.created_at).getTime()) / (1000 * 60)) > 0
-              ? Math.floor((Date.now() - new Date(comment.created_at).getTime()) / (1000 * 60))
-              : 0
-          }}
-          minutes ago</time
-        >
-      </div>
+    </section>
+    <!-- Comments -->
+    <ul class="container mx-auto" v-if="sortedComments.length > 0">
+      <li
+        class="p-6 bg-gray-50 border border-gray-200"
+        v-for="comment in sortedComments"
+        :key="comment.id"
+      >
+        <!-- Comment Author -->
+        <div class="mb-5">
+          <div class="font-bold">{{ comment.users?.name }}</div>
+          <time
+            >{{
+              Math.floor((Date.now() - new Date(comment.created_at).getTime()) / (1000 * 60)) > 0
+                ? Math.floor((Date.now() - new Date(comment.created_at).getTime()) / (1000 * 60))
+                : 0
+            }}
+            minutes ago</time
+          >
+        </div>
 
-      <p>
-        {{ comment.comment }}
-      </p>
-    </li>
-  </ul>
-  <p v-else class="container mx-auto">No comments on song</p>
+        <p>
+          {{ comment.comment }}
+        </p>
+      </li>
+    </ul>
+    <p v-else class="container mx-auto">No comments on song</p>
+  </main>
 </template>
 
 <script setup lang="ts">
